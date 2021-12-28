b21d958d820694023a38673d5662c4cb47dc46c8
Milosz Mizak <mizak.milosz@gmail.com>
2021-12-29 00:49:47 +0100

    found code to implement grid with snapping ships

--- numpy_test.py
+++ numpy_test.py
@@ -1,4 +1,24 @@
 import numpy as np
 
-np_array = np.zeros((10, 10), dtype=np.int16)
-print(np_array)
+square_width = 13
+grid_width = 3
+a = np.zeros((grid_width, grid_width))
+print(a)
+ix = 0
+iy = 0
+i_width = 0
+i_column = 0
+with np.nditer(a, op_flags=['readwrite']) as it:
+    for x in it:
+        x[...] = (ix, iy)
+        ix += square_width
+        i_width += 1
+        if i_width == grid_width:
+            i_column += 1
+            i_width = 0
+            ix = 0
+            iy += square_width
+
+
+
+print(a)
