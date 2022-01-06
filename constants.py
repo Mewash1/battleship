0558a54c0090a659f22a16f9b210c727102bcd24
Milosz Mizak <01169211@pw.edu.pl>
2022-01-06 19:40:22 +0100

    added constants, cleaned up main.py

---
+++ constants.py
@@ -0,0 +1,9 @@
+import pygame
+
+pygame.init()
+SIZE = WIDTH, HEIGHT = 1072, 603
+BACKGROUND = 242, 216, 186
+BLACK = 0, 0, 0
+FONT = pygame.font.SysFont('calibri', 32)
+CLOCK = pygame.time.Clock()
+BATTLESHIP = "./battleships.jpg"
--- main.py
+++ main.py
@@ -1,29 +1,32 @@
 import sys, pygame
-pygame.init()
+import constants
 
-size = width, height = 1072, 603
-background = 242, 216, 186
-black = 0, 0, 0
-font = pygame.font.SysFont('calibri', 32)
-pygame.display.set_caption('Statki')
-text = font.render('Wciśnij ENTER aby rozpocząć', True, black)
-textRect = text.get_rect()
-textRect.center = (width/2, height/2)
-textRect.y += 100
+def main():
+    pygame.init()
 
-screen = pygame.display.set_mode(size)
-title_image = pygame.image.load("battleships.jpg").convert()
-imgrect = title_image.get_rect()
-imgrect.center = (width/2, height/2)
-imgrect.y -= 80
+    pygame.display.set_caption('Statki')
+    text = constants.FONT.render('Wciśnij ENTER aby rozpocząć', True, constants.BLACK)
+    textRect = text.get_rect()
+    textRect.center = (constants.WIDTH/2, constants.HEIGHT/2)
+    textRect.y += 100
 
-screen.fill(background)
-screen.blit(title_image, imgrect)
-screen.blit(text, textRect)
-pygame.display.flip()
+    screen = pygame.display.set_mode(constants.SIZE)
+    title_image = pygame.image.load(constants.BATTLESHIP).convert()
+    imgrect = title_image.get_rect()
+    imgrect.center = (constants.WIDTH/2, constants.HEIGHT/2)
+    imgrect.y -= 80
 
-while 1:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RETURN: sys.exit()
+    screen.fill(constants.BACKGROUND)
+    screen.blit(title_image, imgrect)
+    screen.blit(text, textRect)
+    pygame.display.flip()
+
+    while 1:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN: sys.exit()
+
+
+if __name__ == "__main__":
+    main()
